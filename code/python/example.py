dec1c26ee1451c9d3e1a082fe4f406611bb834e5
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:14:05 +0000

    Updating sandbox wwc2019

---
+++ code/python/example.py
@@ -0,0 +1,23 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (t:Tournament {year: $year})<-[:PARTICIPATED_IN]-(team)
+RETURN team.name as team
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      year="2019").data())
+
+  for record in results:
+    print(record['team'])
+
+driver.close()
