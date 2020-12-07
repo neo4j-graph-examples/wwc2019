dfa3b2759469525f6677de06987583be3c86ec45
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:14:05 +0000

    Updating sandbox wwc2019

---
+++ code/javascript/example.js
@@ -0,0 +1,28 @@
+// npm install --save neo4j-driver
+// node example.js
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('bolt://<HOST>:<BOLTPORT>',
+                  neo4j.auth.basic('<USERNAME>', '<PASSWORD>'), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH (t:Tournament {year: $year})<-[:PARTICIPATED_IN]-(team)
+  RETURN team.name as team
+  `;
+
+const params = {"year": "2019"};
+
+const session = driver.session({database:"neo4j"});
+
+session.run(query, params)
+  .then((result) => {
+    result.records.forEach((record) => {
+        console.log(record.get('team'));
+    });
+    session.close();
+    driver.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
